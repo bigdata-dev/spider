e0bb7b1163e4743fb69c0cbf8b42b9e547d1e26c
tonye0115 <tonye0115>
2016-08-28 09:23:03 +0800

    添加flume

---
+++ src/test/java/com/ryxc/spider/TestLog4j.java
@@ -0,0 +1,23 @@
+package com.ryxc.spider;
+
+import com.ryxc.spider.utils.ThreadUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by tonye0115 on 2016/8/27.
+ */
+public class TestLog4j {
+    Logger logger = LoggerFactory.getLogger(TestLog4j.class);
+    @Test
+    public void test1() {
+        while (true) {
+            logger.info("当前时间戳：{}", System.currentTimeMillis());
+            ThreadUtils.sleep(1000);
+        }
+    }
+
+
+
+}
